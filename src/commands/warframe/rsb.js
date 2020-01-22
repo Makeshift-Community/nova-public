ed6770b252cd979f2703f576004826409fb86359
NKN1396 <someone.took.nkn1396@gmail.com>
2020-01-22 22:37:33 +0100

    Removed clan-related commands

---
+++ src/commands/warframe/rsb.js
@@ -0,0 +1,25 @@
+const Command = require("./../../utils/novaCommand")
+const {stripIndents} = require("common-tags")
+
+module.exports = class extends Command {
+	constructor(client) {
+		super(client, {
+			name: "rsb",
+			aliases: ["wfrsb", "schoolbus", "raidschoolbus", "recruitmentschoolbus"],
+			group: "warframe",
+			description: "Warframe recruitment schoolbus Discord"
+		})
+	}
+
+	async run(message) {
+		try {
+			await message.channel.send(stripIndents`Warframe Recruitment Schoolbus:
+			https://discord.gg/wfrsb
+			*Formerly the Warframe Raid Schoolbus, a server that was dedicated to Warframe raids.
+			Now they're a tight-knit community focused on helping new players out.*`)
+			message.react("✅")
+		} catch (error) {
+			console.error(error)
+		}
+	}
+}
